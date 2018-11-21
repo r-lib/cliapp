e2e4279d162e23ff7621cdbd7cb40159f4ad09a2
Gábor Csárdi <csardi.gabor@gmail.com>
2018-11-21 21:45:11 +0000

    Docs, more to come

---
+++ R/docs.R
@@ -0,0 +1,97 @@
+
+#' CLI inline markup
+#'
+#' @section Command substitution:
+#'
+#' All text emitted by cliapp supports glue interpolation. Expressions
+#' enclosed by braces will be evaluated as R code. See [glue::glue()] for
+#' details.
+#'
+#' In addition to regular glue interpolation, cliapp can also add classes
+#' to parts of the text, and these classes can be used in themes. For
+#' example
+#' 
+#' ```
+#' cli_text("This is {emph important}.")
+#' ```
+#' 
+#' adds a class to the "important" word, class "emph". Note that in this
+#' cases the string within the braces is not a valid R expression. If you
+#' want to mix classes with interpolation, add another pair of braces:
+#' 
+#' ```
+#' adjective <- "great"
+#' cli_text("This is {emph {adjective}}.")
+#' ```
+#'
+#' An inline class will always create a `span` element internally. So in
+#' themes, you can use the `span.emph` CSS selector to change how inline
+#' text is emphasized:
+#' 
+#' ```
+#' cli_div(theme = list(span.emph = list(color = "red")))
+#' adjective <- "nice and red"
+#' cli_text("This is {emph {adjective}}.")
+#' ```
+#'
+#' @section Classes:
+#'
+#' The default theme defines the following inline classes:
+#' * `emph` for emphasized text.
+#' * `strong` for strong importance.
+#' * `code` for a piece of code.
+#' * `pkg` for a package name.
+#' * `fun` for a function name.
+#' * `arg` for a function argument.
+#' * `key` for a keyboard key.
+#' * `file` for a file name.
+#' * `path` for a path (essentially the same as `file`).
+#' * `email` for an email address.
+#' * `url` for a URL.
+#' * `var` for a variable name.
+#' * `envvar` for the name of an environment variable.
+#'
+#' See examples below.
+#'
+#' You can simply add new classes by defining them in the theme, and then
+#' using them, see the example below.
+#'
+#' @name inline-markup
+#' @examples
+#' ## Some inline markup examples
+#' cli_ul()
+#' cli_it("{emph Emphasized} text")
+#' cli_it("{strong Strong} importance")
+#' cli_it("A piece of code: {code sum(a) / length(a)}")
+#' cli_it("A package name: {pkg cliapp}")
+#' cli_it("A function name: {fun cli_text}")
+#' cli_it("A function argument: {arg text}")
+#' cli_it("A keyboard key: press {key ENTER}")
+#' cli_it("A file name: {file /usr/bin/env}")
+#' cli_it("An email address: {email bugs.bunny@acme.com}")
+#' cli_it("A URL: {url https://acme.com}")
+#' cli_it("A variable name: {var mtcars}")
+#' cli_it("An environment variable: {envvar R_LIBS}")
+#' cli_end()
+#'
+#' ## Adding a new class
+#' cli_div(theme = list(
+#'   span.myclass = list(color = "lightgrey"),
+#'   "span.myclass::before" = list(content = "["),
+#'   "span.myclass::after" = list(content = "]")))
+#' cli_text("This is {myclass in brackets}.")
+#' cli_end()
+NULL
+
+#' CLI containers
+#'
+#' TODO
+#'
+#' @name containers
+NULL
+
+#' CLI themes
+#'
+#' TODO
+#' @name themes
+NULL
--- R/package.R
+++ R/package.R
@@ -4,9 +4,19 @@
 #' Create rich command line applications, with colors, headings, lists,
 #' alerts, progress bars, etc. It uses CSS for theming.
 #'
+#' See [themes] for theming, [containers] for container elements,
+#' [inline-markup] for more about command substitution and inline markup.
+#'
+#' See also the various CLI elements:
+#' * Text elements: [cli_text()], [cli_verbatim()], [cli_h1()].
+#' * Containers: [cli_div()], [cli_par()], [cli_end()].
+#' * Lists: [cli_ul()], [cli_ol()], [cli_dl()], [cli_it()].
+#' * Alerts: [cli_alert()].
+#' * Progress bars: [cli_progress_bar()].
+#' 
 #' @docType package
 #' @name cliapp
-NULL
+"_PACKAGE"
 
 cliappenv <- new.env()
 cliappenv$stack <- list()
