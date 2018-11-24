ef3ff39aa9c237619b27500be94024ddfd50a7fa
Gábor Csárdi <csardi.gabor@gmail.com>
2018-11-24 09:27:33 +0000

    Test generating message in subprocess

--- tests/testthat/test-subprocess.R
+++ tests/testthat/test-subprocess.R
@@ -32,6 +32,8 @@ test_that("events are properly generated", {
   expect_equal(msgs[[2]]$type, "h1")
   expect_equal(msgs[[3]]$type, "text")
   expect_equal(msgs[[4]]$type, "end")
+
+  rs$close()
 })
 
 test_that("subprocess with default handler", {
@@ -64,4 +66,56 @@ test_that("subprocess with default handler", {
   expect_equal(msgs[[2]]$type, "h1")
   expect_equal(msgs[[3]]$type, "text")
   expect_equal(msgs[[4]]$type, "end")
+
+  rs$close()
+})
+
+test_that("output in shild process", {
+  ## This needs callr >= 3.0.0.90001, which is not yet on CRAN
+  if (packageVersion("callr") < "3.0.0.9001") skip("Need newer callr")
+
+  do <- function() {
+    options(crayon.enabled = TRUE)
+    options(crayon.colors = 256)
+    crayon::num_colors(forget = TRUE)
+    withCallingHandlers(
+      cliapp_message = function(msg) {
+        withCallingHandlers(
+          cliapp:::cli_server_default(msg),
+          message = function(mmsg) {
+            class(mmsg) <- c("callr_message", "message", "condition")
+            signalCondition(mmsg)
+            invokeRestart("muffleMessage")
+          }
+        )
+        invokeRestart("muffleMessage")
+      }, {
+        cliapp::start_app(theme = cliapp::simple_theme())
+        cliapp::cli_h1("Title")
+        cliapp::cli_text("This is generated in the {emph subprocess}.")
+      }
+    )
+  }
+
+  rs <- callr::r_session$new()
+  on.exit(rs$kill(), add = TRUE)
+
+  msgs <- list()
+  withCallingHandlers(
+    callr_message = function(msg) {
+      msgs <<- c(msgs, list(msg))
+      if (!is.null(findRestart("muffleMessage"))) {
+        invokeRestart("muffleMessage")
+      }
+    },
+    rs$run(do)
+  )
+
+  lapply(msgs, expect_s3_class, "callr_message")
+  str <- paste(vcapply(msgs, "[[", "message"), collapse = "")
+  expect_true(crayon::has_style(str))
+  expect_match(str, "Title")
+  expect_match(str, "This is generated")
+
+  rs$close()
 })
